9645ec64e4cf6aece280db4005452277d622dc02
mac <mac@macs-MacBook-Pro.local>
2024-09-05 14:30:42 +0300

    pages layout

---
+++ src/app/routing/index.tsx
@@ -0,0 +1,20 @@
+import { createBrowserRouter } from "react-router-dom";
+import { MainLayout } from "../../shared/ui/main-layout";
+import { TodoListPage } from "../../pages/todo-list-page";
+
+export const router = createBrowserRouter([
+    {
+        path: "/",
+        element: <MainLayout />,
+        children: [
+            {
+                index: true,
+                element: <TodoListPage />,
+            },
+            {
+                path: ":id",
+                element: <TodoDetailsPage />,
+            },
+        ],
+    },
+]);
